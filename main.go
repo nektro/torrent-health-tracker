904721ce65c82c9de7e6efc348151d65a431212e
Meghan Denny <nektromc@gmail.com>
2019-12-08 22:44:32 -0500

    take in list of tracker and keep ones that are actually online

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+
+	flagTR := pflag.StringArrayP("tracker", "t", []string{}, "")
+
+	pflag.Parse()
+
+	//
+
+	trackers := []string{}
+
+	for i, item := range *flagTR {
+		urlO, _ := url.Parse(item)
+
+		if urlO.Scheme == "http" {
+			req, err := http.NewRequest(http.MethodGet, item, nil)
+			if err != nil {
+				util.LogWarn("tracker:", alias.F("[%d/%d]:", i+1, len(*flagTR)), "bad:", item, err.Error())
+				continue
+			}
+			res, err := client.Do(req)
+			if err != nil {
+				util.LogWarn("tracker:", alias.F("[%d/%d]:", i+1, len(*flagTR)), "bad:", item, err.Error())
+				continue
+			}
+			ioutil.ReadAll(res.Body)
+			util.Log("tracker:", alias.F("[%d/%d]:", i+1, len(*flagTR)), "good:", item)
+			trackers = append(trackers, item)
+
+		} else {
+			util.LogError("tracker:", alias.F("[%d/%d]:", i+1, len(*flagTR)), "unknown scheme:", urlO.Scheme)
+		}
+	}
